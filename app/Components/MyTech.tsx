664dd5c797c1766d53131d376e18f43e2aac9bbd
Nickspero <nspappas99@gmail.com>
2026-02-18 21:26:18 -0500

    more responsiveness fixes

--- app/Components/MyTech.tsx
+++ app/Components/MyTech.tsx
@@ -1,15 +1,35 @@
 const MyTech = () => {
   const techStack = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React",
-    "Firebase",
-    "NextJS",
-    "VSCode",
-    "TailwindCSS",
-    "Figma",
-    "Vue",
+    {title: "HTML",
+      img: "/HTML.png"
+    },
+    {title: "CSS",
+      img: "/CSS.png"
+    },
+    {title: "JavaScript",
+      img: "/JavaScript.png"
+    },
+    {title: "React",
+      img: "/React.png"
+    },
+    {title: "Firebase",
+      img: "/Firebase.png"
+    },
+    {title: "NextJS",
+      img: "/NextJS.png"
+    },
+    {title: "VS Code",
+      img: "/VSCode.png"
+    },
+    {title: "Tailwind CSS",
+      img: "/TailwindCSS.png"
+    },
+    {title: "Figma",
+      img: "/Figma.png"
+    },
+    {title: "Vue",
+      img: "/Vue.png"
+    }
   ];
 
   return (
@@ -20,8 +40,8 @@ const MyTech = () => {
             <div className="grid grid-cols-5 justify-items-center my-4 gap-4">
               {techStack.map((tech, index) => (
                 <figure className="tech" key={index}>
-                  <img className="tech__logo" src={`/${tech}.png`} alt="" />
-                  <p className="tech__name text-[12px] md:text-[16px]">{tech}</p>
+                  <img className="tech__logo" src={tech.img} alt="" />
+                  <p className="tech__name text-[12px] md:text-[16px]">{tech.title}</p>
                 </figure>
               ))}
             </div>
